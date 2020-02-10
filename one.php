ff22e245b6f9ed71a9b789277757b3cfe914a78e
cwt2030 <cwt2030@gmail.com>
2020-02-10 18:56:04 +0300

    v6

--- one.php
+++ one.php
@@ -8,9 +8,24 @@
 
 
     <?php
+    define("M5","multiple of 5");
+    define("MOTM5","not a multiple of 5 " , true);
+    $num1 =1;
+    while($num1<15)
+    {
+        if ($num1<15)
+        {
+        echo $num1."is".M5."\n";
+    }
+    elseif ($num1 % 2 !=0)
+    {
+        echo $num1."is".MOTM5."\n";
+    }
+    $num1++;
+    }
 
         echo "Hello sultan!";
-        
+
         ?>
     </body>
 </html>
